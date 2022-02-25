aea41bd282db97c064b0d8f15fd57b9eff8e623c
Mauricio Vásquez <mauricio@kinvolk.io>
2022-02-25 17:01:37 -0500

    libbpf-tools: Add experimental integration with BTFGen
    This commit adds an experimental BTFGen[0] integration to allow some of
    the libbpf-tools to run in systems that don't provide BTF information.
    
    The whole process consist of two parts: (1) generating and embedding the
    BTF file within the tools binary and (2) using those when the tool is
    run.
    
    The first part is done by using the Makefile, it generates the reduced
    BTF files for the different eBPF objects of the tools, those files are
    then compressed and a C header file with its content is created.
    
    The second part is handled by a new C file that provides the logic to
    uncompress and save the BTF file according to the Linux distribution and
    kernel version where the tools is being run.
    
    [0] https://lore.kernel.org/bpf/20220215225856.671072-1-mauricio@kinvolk.io/
    
    Signed-off-by: Mauricio Vásquez <mauricio@kinvolk.io>

---
+++ libbpf-tools/min_core_btf_helpers.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+
+#ifndef __MIN_CORE_BTFS_H
+#define __MIN_CORE_BTFS_H
+
+#include <bpf/libbpf.h>
+
+int save_min_core_btf(char *template);
+
+#endif /* __MIN_CORE_BTFS_H */
